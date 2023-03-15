267468526f3520cd2d8af510e0a7879e7ddcc37c
ImNotAuzero <liam@liampbennett.me>
2023-03-15 18:47:09 +0000

    Update logger
    Add missing method because I'm an idiot and forgot to add it.

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -6,6 +6,12 @@ module.exports.log = (...args) => {
   console.log(`${chalk.cyan.bold(`[${this._timestamp()}]`)} ${args}`);
 }
 
+module.exports.error = (...args) => {
+  console.log(chalk.red.bold(`[ERROR] [${this._timestamp()}]`));
+  console.warn(...message);
+  console.log(chalk.red.bold(`[/ERROR]`));
+};
+
 module.exports.important = (...args) => {
   console.log(`${chalk.red.bold(`[${this._timestamp()}]`)} ${args}`);
 }
